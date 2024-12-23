f4231704b32449c316981ab9f547785b30e5f0e4
Krithick Jegatheesan <j.krithick@gmail.com>
2024-12-23 18:17:48 +0530

    serializer modify according to superuser

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Article, Comment
+from .models import Article, Comment, User
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +10,10 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'is_owner', 'is_admin', 'is_member']
+        read_only_fields = ['is_owner']  # Prevent is_owner from being included in input
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,8 +1,30 @@
 from rest_framework import viewsets
-from .models import Article, Comment
-from .serializers import ArticleSerializer, CommentSerializer
+from .models import Article, Comment, User
+from .serializers import ArticleSerializer, CommentSerializer, UserSerializer
 from .permissions import IsOwner, IsAdmin, IsMember
 
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            # Only superusers can create users
+            return [IsAdmin()]
+        elif self.action in ['update', 'destroy']:
+            # Owners or superusers can update or delete users
+            return [IsOwner() | IsAdmin()]
+        return []  # No special permissions for listing or retrieving users
+
+    def perform_create(self, serializer):
+        # Check if this is the first user being created
+        if User.objects.count() == 0:
+            # Set the first user as the Owner
+            serializer.save(is_owner=True)
+        else:
+            # For all other users, set is_owner as False
+            serializer.save(is_owner=False)
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
